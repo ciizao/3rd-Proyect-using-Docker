7b3f7746992fc254fc9ca7eca343dea4ee89a9e7
ciizao <ciizao@uce.edu.ec>
2024-11-11 17:19:06 -0500

    refactor: :recycle: Change message in web

--- src/main/java/com/example/demo/controller/Exercise3Controller.java
+++ src/main/java/com/example/demo/controller/Exercise3Controller.java
@@ -7,7 +7,7 @@ import org.springframework.web.bind.annotation.RestController;
 public class Exercise3Controller {
     @GetMapping
     public String hello() {
-        return "Hello  World";
+        return "Hello World";
     }
 }
 
