70059cf6ccfeee10cb92b256221dc610e5738815
ciizao <ciizao@uce.edu.ec>
2024-11-11 06:10:04 -0500

    chore: :recycle: Restart files

--- exercise3/obj/Release/net8.0/exercise3.AssemblyInfo.cs
+++ exercise3/obj/Release/net8.0/exercise3.AssemblyInfo.cs
@@ -11,7 +11,7 @@ using System;
 using System.Reflection;
 
 [assembly: System.Reflection.AssemblyCompanyAttribute("exercise3")]
-[assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
+[assembly: System.Reflection.AssemblyConfigurationAttribute("Release")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
 [assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+8ba1360c9ac287f74ac69377df62a8c63b58d1d6")]
 [assembly: System.Reflection.AssemblyProductAttribute("exercise3")]
